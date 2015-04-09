3117659e11e0bfe266ec9a8afcacfc902d98a1b5
panjizhi <panjizhi1987@gmail.com>
2015-04-09 11:27:28 +0800

    Update hijack_document_cookie.js

--- hijack_document_cookie.js
+++ hijack_document_cookie.js
@@ -6,30 +6,78 @@
 //  Browsers：IE11/10/9, Firefox 36.0.4
 //
 ;(function(){
-    var _set, _get,
-        _cDescriptor = Object.getOwnPropertyDescriptor(document, 'cookie') || // chrome 41.0.2272.118 m, IE8(Internet Explorer 8 standards mode supports DOM objects but not user-defined objects. The enumerable and configurable attributes can be specified, but they are not used.)
-                       Object.getOwnPropertyDescriptor(Object.getPrototypeOf(document), 'cookie') || // firefox 36.0.4, IE9/10
-                       Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');// IE11
+    var nativeProperty = {},
+        hijackProperty = {},
+        nativeSetter, 
+        nativeGetter, 
+                       // type: data descriptor
+                       // browsers: chrome 41.0.2272.118 m, IE8
+                       // 
+                       // Internet Explorer 8 standards mode supports DOM objects but not user-defined objects. 
+                       // The enumerable and configurable attributes can be specified, but they are not used.
+        descriptor = Object.getOwnPropertyDescriptor(document, 'cookie') || 
+                       // type: accessor descriptor
+                       // browsers: firefox 36.0.4, IE9/10
+                       Object.getOwnPropertyDescriptor(Object.getPrototypeOf(document), 'cookie') || 
+                       // type: accessor descriptor
+                       // browsers: IE11
+                       Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
                        
-    if(_cDescriptor){
-        if(_cDescriptor.hasOwnProperty('value')){       // chrome 41.0.2272.118 m
-            Object.defineProperty(document, 'cookie', { //data descriptor
-                value: _cDescriptor.value,
-                writable: _cDescriptor.writable,
-                enumerable: _cDescriptor.enumerable,
-                configurable: false
-            });
+    function hijacking(newValue, type){
+        type = type || 'get';
+        console.log('Cookies hijacking(' + type + '): \n' + newValue);        
+    }
+    
+    function nativeCall(newValue){
+        Object.defineProperty(document, 'cookie', { // data descriptor
+                value: descriptor.value,
+                writable: descriptor.writable,
+                enumerable: descriptor.enumerable,
+                configurable: descriptor.configurable
+        });        
+        document.cookie = newValue;
+        
+        descriptor = Object.getOwnPropertyDescriptor(document, 'cookie');
+        Object.defineProperty(document, 'cookie', hijackProperty);
+    }
+    
+    if(descriptor){
+        if(descriptor.hasOwnProperty('value')){ // chrome 41.0.2272.118 m
+            nativeProperty = { // data descriptor
+                value: descriptor.value,
+                writable: descriptor.writable,
+                enumerable: descriptor.enumerable,
+                configurable: descriptor.configurable
+            };
+            hijackProperty = { // accessor descriptor
+                value: '',
+                writable: true,
+                get: function(){
+                    var reserved = descriptor.value;
+                    hijacking(reserved, 'get');
+                    return reserved;
+                },
+                set: function(newValue){
+                    hijacking(newValue, 'set');
+                    nativeCall(newValue);
+                },
+                enumerable: true,
+                configurable: true
+            };
+            Object.defineProperty(document, 'cookie', nativeProperty);
         }else{ // accessor descriptor
-            _get = _cDescriptor.get || function(){console.log('Error!')};
-            _set = _cDescriptor.set || function(){console.log('Error!')};
+            nativeGetter = descriptor.get || function(){console.log('Error!')};
+            nativeSetter = descriptor.set || function(){console.log('Error!')};
             Object.defineProperty(document, 'cookie', {
                 get: function () {
-                    console.log('hijack get cookie action!');
-                    return _get.call(document);
+                    var ret = nativeGetter.call(document);
+                    hijacking(ret, 'get');
+                    return ret;
                 },
                 set: function (value) {
-                    console.log('hijack set cookie action!');
-                    return _set.call(document, value);
+                    var ret = nativeSetter.call(document, value);
+                    hijacking(value, 'set');
+                    return ret;
                 },
                 enumerable: true,
                 configurable: false
@@ -39,6 +87,9 @@
 })();
 
 // test code snippet 
-console.log(document.cookie);
-document.cookie = "hj_author=x3xtxt";
-console.log(document.cookie);
+var data = new Date().getTime();
+document.cookie;
+document.cookie = 'hj_author'+data+'=x3xtxt';
+document.cookie;
+document.cookie = 'x3xtxt'+data+'=hj_author';
+document.cookie;
